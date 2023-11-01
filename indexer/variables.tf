88f8bdf864bd2e4b5c6febef3155ac810811fb6b
Vuong <tranvuong0402@gmail.com>
2023-11-01 16:36:04 +0700

    Resolve conflict

--- indexer/variables.tf
+++ indexer/variables.tf
@@ -42,7 +42,7 @@ variable "environment" {
 
   validation {
     condition = contains(
-      ["dev", "dev2", "dev3", "dev4", "dev5", "staging", "testnet", "public-testnet", "testnet1", "testnet2", "mainnet"],
+      ["dev", "dev2", "dev3", "dev4", "dev5", "staging", "testnet", "public-testnet", "testnet1", "testnet2", "testnet9", "mainnet"],
       var.environment
     )
     error_message = "Err: invalid environment. Must be one of {dev | dev2 | dev3 | dev4 | dev5 | staging | testnet | public-testnet | testnet1 | testnet2 | mainnet}."
