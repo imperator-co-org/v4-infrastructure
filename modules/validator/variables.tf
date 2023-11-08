8e117b7708a36fd4cfb8d3b60031d5cb511b8430
Vuong <tranvuong0402@gmail.com>
2023-11-08 18:20:51 +0700

    Resolve conflict

--- modules/validator/variables.tf
+++ modules/validator/variables.tf
@@ -7,11 +7,7 @@ variable "environment" {
 
   validation {
     condition = contains(
-<<<<<<< HEAD
-      ["dev", "dev2", "dev3", "dev4", "dev5", "staging", "testnet", "public-testnet", "research", "testnet1", "testnet2", "mainnet"],
-=======
       ["dev", "dev2", "dev3", "dev4", "dev5", "staging", "testnet", "public-testnet", "research", "testnet1", "testnet2", "testnet9", "mainnet"],
->>>>>>> main
       var.environment
     )
     error_message = "Err: invalid environment. Must be one of {dev | dev2 | dev3 | dev4 | dev5 | staging | testnet | public-testnet | research | testnet1 | testnet2 | mainnet}."
