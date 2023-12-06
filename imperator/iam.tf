02f059f3a1524a2c5d08e4997cb1836b0715963b
Vuong <tranvuong0402@gmail.com>
2023-12-06 11:39:12 +0700

    Update IAM role for mainnet github action

--- imperator/iam.tf
+++ imperator/iam.tf
@@ -1,4 +1,5 @@
 module "iam_github_actions" {
+  count       = var.environment == "mainnet" ? 0 : 1
   source      = "../modules/iam/github_actions_role"
   name        = "validator"
   environment = var.environment
@@ -7,6 +8,20 @@ module "iam_github_actions" {
     "arn:aws:ecr:us-east-2:${local.account_id}:repository/${var.environment}-validator-snapshot",
     "arn:aws:ecr:us-east-2:${local.account_id}:repository/${var.environment}-orb"
   ]
-  lambda_arns = []
+  lambda_arns      = []
+  assume_iam_roles = []
+}
+
+module "iam_github_actions_mainnet" {
+  count       = var.environment == "mainnet" ? 1 : 0
+  source      = "../modules/iam/github_actions_role"
+  name        = "validator"
+  environment = var.environment
+  ecr_repository_arns = [
+    "arn:aws:ecr:ap-northeast-1:${local.account_id}:repository/${var.environment}-validator",
+    "arn:aws:ecr:ap-northeast-1:${local.account_id}:repository/${var.environment}-validator-snapshot",
+    "arn:aws:ecr:ap-northeast-1:${local.account_id}:repository/${var.environment}-orb"
+  ]
+  lambda_arns      = []
   assume_iam_roles = []
 }
