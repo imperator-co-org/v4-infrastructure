0d6dddd6ec8bb71a5fe6b1165b8585d64bc9d25f
Vuong <tranvuong0402@gmail.com>
2024-09-25 10:03:52 +0700

    Fix arn of rds role

--- indexer/rds.tf
+++ indexer/rds.tf
@@ -247,7 +247,7 @@ resource "aws_db_instance" "main" {
   auto_minor_version_upgrade            = false
   multi_az                              = var.enable_rds_main_multiaz
   monitoring_interval                   = 60
-  monitoring_role_arn                   = aws_iam_role.rds_enhanced_monitoring_role
+  monitoring_role_arn                   = aws_iam_role.rds_enhanced_monitoring_role.arn
 
   tags = {
     Name        = local.aws_db_instance_main_name
